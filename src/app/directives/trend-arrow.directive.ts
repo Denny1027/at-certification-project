f17025d97233061d2791afcf014ed583d93eed59
Denny1027 <47983284+Denny1027@users.noreply.github.com>
2022-12-30 09:51:48 +0100

    1 file modified

--- src/app/directives/trend-arrow.directive.ts
+++ src/app/directives/trend-arrow.directive.ts
@@ -11,7 +11,7 @@ import {
 })
 export class TrendArrowDirective implements OnChanges {
   @HostBinding('style.color') color = 'grey';
-  @HostBinding('style.font-size') fontSize = '30px';
+  @HostBinding('style.font-size') fontSize = '100px';
 
   @Input('appTrendArrow') value?: number = 0;
 
@@ -20,13 +20,13 @@ export class TrendArrowDirective implements OnChanges {
   ngOnChanges(): void {
     if (this.value) {
       if (this.value > 0) {
-        this.elementRef.nativeElement.innerHTML = 'Freccia SU';
+        this.elementRef.nativeElement.innerHTML = '🡅';
         this.color = 'green';
       } else if (this.value === 0) {
-        this.elementRef.nativeElement.innerHTML = 'Uguale';
+        this.elementRef.nativeElement.innerHTML = '=';
         this.color = 'grey';
       } else if (this.value < 0) {
-        this.elementRef.nativeElement.innerHTML = 'Freccia GIU';
+        this.elementRef.nativeElement.innerHTML = '🡇';
         this.color = 'red';
       } else {
         this.elementRef.nativeElement.innerHTML = '';
